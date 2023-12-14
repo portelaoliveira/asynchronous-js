eddad8cbed3d8603494afe02185f9f2c4cfda91d
Portela <daniloportela97@gmail.com>
2023-12-14 09:28:57 -0300

    Tratando erros em promises.

--- promisified.js
+++ promisified.js
@@ -2,8 +2,10 @@
 //friends => ['joão', 'bruna', 'felipe', 'Lira'];
 //photos <= ['egito', 'bahamas', 'maldivas', 'suécia']
 
+const clc = require("cli-color");
+
 function getUser() {
-  console.log("Recuperando informações do usuário");
+  console.log(clc.green("Recuperando informações do usuário"));
   const promise1 = new Promise(resolve => {
     setTimeout(() => {
       resolve({ id: 0, name: "Daniel", surname: "Porto" });
@@ -14,7 +16,10 @@ function getUser() {
 
 function getFriendList(user) {
   console.log(
-    `Recuperando lista de amigos do usuário ${user.name} ${user.surname}`
+    clc.green(
+      "Recuperando lista de amigos do usuário ",
+      clc.blue(`${user.name} ${user.surname}`)
+    )
   );
   const promise2 = new Promise(resolve => {
     setTimeout(() => {
@@ -24,11 +29,33 @@ function getFriendList(user) {
   return promise2;
 }
 
+// function getPhotos(username) {
+//   console.log(
+//     clc.green(
+//       "Recuperando lista de fotos do usuário ",
+//       clc.blue(`${username}`)
+//     )
+//   );
+//   const promise3 = new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(["egito", "bahamas", "maldivas", "suécia"]);
+//     }, 2000);
+//   });
+//   return promise3;
+// }
+
 function getPhotos(username) {
-  console.log(`Recuperando lista de fotos do usuário ${username}`);
-  const promise3 = new Promise(resolve => {
+  console.log(
+    clc.green(
+      "Recuperando lista de fotos do usuário ",
+      clc.blue(`${username}`)
+    )
+  );
+  const promise3 = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(["egito", "bahamas", "maldivas", "suécia"]);
+      reject(
+        "Não foi possível acessar as fotos. Usuário desabilitou o perfil."
+      );
     }, 2000);
   });
   return promise3;
@@ -37,4 +64,8 @@ function getPhotos(username) {
 getUser()
   .then(user => getFriendList(user))
   .then(friends => getPhotos(friends[3]))
-  .then(photos => console.log(photos));
+  .then(photos => console.log(clc.blue(photos)))
+  .catch(error => console.log(clc.red(error)))
+  .finally(() => {
+    console.log(clc.yellow("Esse é o fim da comunicação com o instagram."));
+  });
